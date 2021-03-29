c0e47c29984f0ef9b869dba2ad91110c414653b3
=hanipig <17802031896@163.com>
2021-03-29 23:15:18 +0800

    防抖实现

---
+++ demo/防抖/debounce.js
@@ -0,0 +1,67 @@
+/*
+ * @Description: 
+ * @Autor: hanipig
+ * @Date: 2021-03-29 21:25:13
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-03-29 23:06:43
+ */
+var count = 1;
+var container = document.getElementById('container');
+var cancelBtn = document.getElementById('cancelBtn');
+
+function getUserAction (e) {
+  console.log(this);//未节流时指向目标元素   此时指向window
+  console.log(e)//未节流时指向原生事件对象   此时undefined
+  container.innerHTML = count++;
+  return "返回值";
+};
+
+//未进行节流
+// container.onmousemove = getUserAction;
+let debounced = debounce(getUserAction, 10000, true);
+
+container.onmousemove = debounced;
+
+
+cancelBtn.addEventListener("click", function () {
+  debounced.cancel();
+})
+
+//第一版
+// this指向window
+
+function debounce (fn, wait, immediate) {
+  let timer,
+    res;
+
+  let debounced = function () {//返回一个函数，相当于这个返回的函数就是之前的getUserAction，所以this指向目标对象、arguments对象可以获取到事件对象
+    let context = this;
+    let args = arguments;
+
+    if (timer) clearTimeout(timer);//clearTimerout 后timer的值还是之前那个句柄，为truely值
+
+    if (immediate) {
+      //如果之前未执行过，则!timer为true 即立即执行
+      let callNow = !timer;
+      timer = setTimeout(function () {
+        timer = null;//每次把timer设置为null，确保每下一次能够立即执行
+        console.log("可以再次触发了");
+      }, wait)
+      if (callNow) {
+        res = fn.apply(context, args);
+      }
+    } else {
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, wait);
+    }
+    return res;
+  }
+
+  debounced.cancel = function () {
+    clearTimeout(timer);
+    timer = null;
+  }
+
+  return debounced;
+}
